3829a2f0be61d0f7197244f38f9be67f900e9b1d
Prakriti <rajbhandariprakriti.7@gmail.com>
2020-07-09 19:47:05 +1000

    Updates

---
+++ precache-manifest.f05ad0491b58764c5ad25035cc89fdc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416faa0cc40c364e68ba29ffcd4cb691",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "5e9ed97b8e0b8b620a55",
+    "url": "/User-Directory/static/css/main.4fd5818c.chunk.css"
+  },
+  {
+    "revision": "4df788a3cc9f26f691ec",
+    "url": "/User-Directory/static/js/2.6cd7e56d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.6cd7e56d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9ed97b8e0b8b620a55",
+    "url": "/User-Directory/static/js/main.b64ac85e.chunk.js"
+  },
+  {
+    "revision": "424f344ea7a208d9dec2",
+    "url": "/User-Directory/static/js/runtime-main.512c965c.js"
+  }
+]);
